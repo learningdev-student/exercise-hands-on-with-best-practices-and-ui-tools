17cb1e123fc3a808050260820766eab40ca82f76
learningdev-student <learningdev.student@gmail.com>
2026-06-16 19:25:50 +0300

    Adding unit tests for basic calculator

---
+++ tests/test_calculator.py
@@ -0,0 +1,11 @@
+from src.calculator import add, substract
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, -1) == -2
+    assert add(0, 0) == 0
+
+def test_substract():
+    assert substract(5, 3) == 2
+    assert substract(-1, -1) == 0
+    assert substract(0, 5) == -5
