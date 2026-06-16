7ce54c35512184cfe74a4d676a2a092a6565742d
learningdev-student <learningdev.student@gmail.com>
2026-06-16 19:33:59 +0300

    Adding and initial formatiing of the files

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,9 +1,11 @@
 from src.calculator import add, substract
 
+
 def test_add():
     assert add(1, 2) == 3
     assert add(-1, -1) == -2
     assert add(0, 0) == 0
+
 
 def test_substract():
     assert substract(5, 3) == 2
