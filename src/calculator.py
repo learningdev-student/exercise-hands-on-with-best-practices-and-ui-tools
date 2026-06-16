d3709f0c6527f3e32f4a419d358a5b04c20c676f
learningdev-student <learningdev.student@gmail.com>
2026-06-16 20:36:08 +0300

    Trying to ignore the pre-commit hooks

--- src/calculator.py
+++ src/calculator.py
@@ -2,16 +2,13 @@ def add(first_number: int, second_number: int):
     """Return the sum of two numbers."""
     return first_number + second_number
 
-
 def substract(first_number: int, second_number: int):
     """Return the difference of two numbers."""
     return first_number - second_number
 
-
 def multiply(first_number: int, second_number: int):
     """Return the product of two numbers."""
     return first_number * second_number
-
 
 def divide(first_number: int, second_number: int):
     """Return the quotient of two numbers."""
