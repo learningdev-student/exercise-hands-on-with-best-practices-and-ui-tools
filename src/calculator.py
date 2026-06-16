a24c71050693a46c3a631ebf2c17f4adb99ca120
learningdev-student <learningdev.student@gmail.com>
2026-06-16 19:55:26 +0300

    Testing the pre-commit hook

--- src/calculator.py
+++ src/calculator.py
@@ -3,9 +3,17 @@ def add(first_number: int, second_number: int):
     return first_number + second_number
 
 
+
+
+
 def substract(first_number: int, second_number: int):
     """Return the difference of two numbers."""
     return first_number - second_number
+
+
+
+
+
 
 
 def multiply(first_number: int, second_number: int):
