9a6e531b7cabfbcbaf463ef078fb33eaed0b47cc
learningdev-student <learningdev.student@gmail.com>
2026-06-16 19:57:23 +0300

    Add scripts

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,14 +1,17 @@
 from src.calculator import add, substract, multiply
 
+
 def test_add():
     assert add(1, 2) == 3
     assert add(-1, -1) == -2
     assert add(0, 0) == 0
 
+
 def test_substract():
     assert substract(5, 3) == 2
     assert substract(-1, -1) == 0
     assert substract(0, 5) == -5
+
 
 def test_multiply():
 
