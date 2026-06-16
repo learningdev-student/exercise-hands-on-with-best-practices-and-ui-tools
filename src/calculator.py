d4f297724d2599f28cb8a4165e05e6e9eb012411
learningdev-student <learningdev.student@gmail.com>
2026-06-16 19:14:48 +0300

    Adding changes related to basic calculator logic

---
+++ src/calculator.py
@@ -0,0 +1,8 @@
+def add(first_number: int, second_number: int):
+    """Return the sum of two numbers."""
+    return first_number + second_number
+
+
+def substract(first_number: int, second_number: int):
+    """Return the difference of two numbers."""
+    return first_number - second_number
